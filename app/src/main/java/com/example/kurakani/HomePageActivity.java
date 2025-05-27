14d5295e920c0f5b8edf70e0718bb6391513b5fd
Bishal <bishalyadav500@gmail.com>
2025-05-27 13:35:43 +0545

    set universal font family and updated profilepage in HomePageActivity

--- app/src/main/java/com/example/kurakani/HomePageActivity.java
+++ app/src/main/java/com/example/kurakani/HomePageActivity.java
@@ -1,11 +1,17 @@
 package com.example.kurakani;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 public class HomePageActivity extends AppCompatActivity {
+
+    ImageView v_profile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +19,22 @@ public class HomePageActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_home_page);
 
+        findview();
+
+        v_profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.fragmentContainer, new MainProfile())
+                        .addToBackStack(null)
+                        .commit();
+            }
+        });
+    }
+    private void findview(){
+        v_profile = findViewById(R.id.v_profile);
 
     }
+
+
 }
